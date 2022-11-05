fc28c10d052cb91e726c48b4022f09a756da8beb
adi-shankaracharya <adi.shank.101@gmail.com>
2022-11-05 18:41:27 -0400

    copy and paste w map

--- config.js
+++ config.js
@@ -1,4 +1,26 @@
 var config = {
+    "style": "mapbox://styles/adi-shank-101/cla4hiec0000414pelasji86q",
+    "accessToken": "pk.eyJ1IjoiYWRpLXNoYW5rLTEwMSIsImEiOiJjbDlnMzE3OG0wZnI0NDJteDdubjZuN2dzIn0.ENwgy7R0cb5OrJ49PM-3Pw",
+    "showMarkers": false,
+    "theme": "light",
+    "alignment": "right",
     "title": "The Life of Adi Shankaracharya - A 101",
-    "theme": "light"
+    "chapters": [
+        {
+            "id": "chapter_0",
+            "title": "BIRTH",
+            "alignment": "center",
+            "description": "Shankaracharya was born in the town of <b>Kaladi</b>.",
+            "location": {
+                "center": {
+                "lon": 75.81537,
+                "lat": 31.16460
+                },
+                "zoom": 2.66,
+                "speed": 0.4,
+                "pitch": 0,
+                "bearing": 0
+            }
+        }
+    ]
 }
